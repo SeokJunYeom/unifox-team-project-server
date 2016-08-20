9e4dbff8769a2e761052e582fe448f44202a6d2d
seokjunyeom <tjrwnsdl0720@naver.com>
2016-08-21 06:40:34 +0900

    'imgProcess.py' modify

--- imgProcess.py
+++ imgProcess.py
@@ -2,18 +2,20 @@ import cv2
 import numpy
 
 class ImgProcess:
-	def imgDecode(img_str):
-		data = numpy.fromstring(img_str, dtype = 'unint8')
+	def imgDecode(imgStr):
+		data = numpy.fromstring(imgStr, dtype = 'unint8')
 		img = cv2.imdecode(data, 1)
 
 		return img
 
 	def imgToString(imgName):
-		fd = open("image/" + imgName, 'rb')
-		img_str = str(fd.read())
-		fd.close()
+		img = cv2.imread("image/" + imgName)
+		imgStr = cv2.imencode('.jpg', img)[1].tostring()
 
-		return img_str
+		imgLen = str(len(imgStr))
+		imgStr = "image" + "*" imgName + "*" + imgLen + "*" + imgStr
+
+		return imgStr
 
 	def imgSave(imgName, img):
 		cv2.imwrite("image/" + imgName, img)
